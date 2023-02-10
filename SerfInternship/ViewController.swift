2b25a9033489ad0c11c8b88831856ba2d4efc6bc
NikitaRekaev <nvvvvk@gmail.com>
2023-02-10 16:31:23 +0400

    Initial Commit

---
+++ SerfInternship/ViewController.swift
@@ -0,0 +1,15 @@
+//  ViewController.swift
+//  Nikita Rekaev 10.02.2023
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
